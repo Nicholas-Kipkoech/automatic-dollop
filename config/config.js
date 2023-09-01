1d66e9297d3b59ea1c82c1cf1eb08e8926223ae4
Nicholas Kipkoech Ngetich <58167221+Nicholas-Kipkoech@users.noreply.github.com>
2023-09-01 10:56:10 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ let DB_URI = process.env.MONGO_URI
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(DB_URI)
+        await mongoose.connect("mongodb+srv://Nickey:Nickey@cluster0.i0gut.mongodb.net/Towgig?retryWrites=true&w=majority)
         console.log("Database connected...")
     } catch (error) {
         console.log("something went wrong....", error)
